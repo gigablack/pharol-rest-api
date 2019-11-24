be4de031a0875a45d3e3a1e64dabb4fdb05984d2
Gustavo Bethelmy <tenchu96@gmail.com>
2019-11-24 12:05:17 -0430

    basic crud ready

--- src/people/people.controller.ts
+++ src/people/people.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Res } from '@nestjs/common';
+import { PeopleDTO } from './dto/people.dto';
+import { Response } from 'express';
+import { PeopleService } from './people.service';
 
 @Controller('people')
-export class PeopleController {}
+export class PeopleController {
+    constructor(private peopleService: PeopleService) {}
+
+    @Get()
+    async getAllPeople(@Res() res: Response) {
+        // return all people
+        return res.json(await this.peopleService.getAllPeople());
+    }
+
+    @Post('/create')
+    async createPerson(@Body() person: PeopleDTO, @Res() res: Response) {
+        // create a person
+        return res.json(await this.peopleService.createPerson(person));
+    }
+
+    @Get('/:id')
+    async getPerson(@Param('id') id: string, @Res() res: Response) {
+        // return one person
+        return res.json(await this.peopleService.getPerson(id));
+    }
+
+    @Put('/:id')
+    async updatePerson(@Param('id') id: string, @Body() person: PeopleDTO, @Res() res: Response) {
+        // update person
+        return res.json(await this.peopleService.updatePerson(id, person));
+    }
+
+    @Delete('/:id')
+    async deletePerson(@Param('id') id: string, @Res() res: Response) {
+        // delete person
+        return res.json(await this.peopleService.deletePerson(id));
+    }
+}
